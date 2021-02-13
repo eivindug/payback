824da8c0ff53f232ccb97094d07cd1100370e559
eivindug <eivindug@stud.ntnu.no>
2021-02-13 20:50:33 +0100

    initialize spring

---
+++ src/test/java/no/visma/payback/PaybackApplicationTests.java
@@ -0,0 +1,13 @@
+package no.visma.payback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
